9963205138bb606135c137e2f2156494c9662785
tniita <takuya2230@gmail.com>
2022-08-17 12:32:45 +0900

    WIP

--- internal/log/segment.go
+++ internal/log/segment.go
@@ -4,6 +4,10 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+
+	"google.golang.org/protobuf/proto"
+
+	api "github.com/tniita/proglog/api/v1"
 )
 
 type segment struct {
@@ -38,4 +42,62 @@ func newSegment(dir string, baseOffset uint64, c Config) (*segment, error) {
 		s.nextOffset = baseOffset + uint64(off)
 	}
 	return s, nil
+}
+
+func (s *segment) Append(record *api.Record) (offset uint64, err error) {
+	cur := s.nextOffset
+	record.Offset = cur
+	p, err := proto.Marshal(record)
+	if err != nil {
+		return 0, err
+	}
+	_, pos, err := s.store.Append(p)
+	if err != nil {
+		return 0, err
+	}
+	if err := s.index.Write(uint32(s.nextOffset-uint64(s.baseOffset)), pos); err != nil {
+		return 0, err
+	}
+	return cur, nil
+}
+
+func (s *segment) Read(off uint64) (*api.Record, error) {
+	_, pos, err := s.index.Read(int64(off - s.baseOffset))
+	if err != nil {
+		return nil, err
+	}
+	p, err := s.store.Read(pos)
+	if err != nil {
+		return nil, err
+	}
+	record := &api.Record{}
+	err = proto.Unmarshal(p, record)
+	return record, err
+}
+
+func (s *segment) IsMaxed() bool {
+	return s.store.size >= s.config.Segment.MaxIndexBytes || s.index.size >= s.config.Segment.MaxIndexBytes || s.index.isMaxed()
+}
+
+func (s *segment) Remove() error {
+	if err := s.Close(); err != nil {
+		return err
+	}
+	if err := os.Remove(s.index.Name()); err != nil {
+		return err
+	}
+	if err := os.Remove(s.store.Name()); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *segment) Close() error {
+	if err := s.index.Close(); err != nil {
+		return err
+	}
+	if err := s.store.Close(); err != nil {
+		return err
+	}
+	return nil
 }
